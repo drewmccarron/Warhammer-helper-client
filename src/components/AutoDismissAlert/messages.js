5b6120d54f3f25a8e50c40b42413fa789094bbf2
drewmccarron <drewcmccarron@gmail.com>
2020-04-03 01:45:46 -0400

    Added failure messages for crud actions

--- src/components/AutoDismissAlert/messages.js
+++ src/components/AutoDismissAlert/messages.js
@@ -7,6 +7,8 @@ export default {
   changePasswordSuccess: 'Password changed successfully!',
   changePasswordFailure: 'Failed to change passwords. Check your old password and try again.',
   deleteSuccess: 'Resource deleted successfully.',
+  deleteFailure: 'Failed to delete resource. It may belong to another user.',
   patchSuccess: 'Succesfully saved changes.',
+  patchFailure: 'Failed to save changes to resource. It may belong to another user.',
   postSuccess: 'Succesfully saved new resource.'
 }
